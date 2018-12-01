7a77671d854bf76e5468413ae9efa295dd898aed
Jesse Mendoza <33880932+jessmend815@users.noreply.github.com>
2018-12-01 00:46:12 -0800

    Buy Time Function

---
+++ Assets/Scripts/ShopController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopController : MonoBehaviour {
+
+	public float blood = PlayerController.Player.blood;
+	public float currTime = PlayerController.Player.time;
+
+	public void BuyTimeS(int cost)
+	{
+		if (blood <= cost)
+		{
+			time += 15;
+		}
+	}
+
+	public void BuyTimeM(int cost)
+	{
+		if (blood <= cost)
+		{
+			time += 30;
+		}
+	}
+
+	public void BuyTimeL(int cost)
+	{
+		if (blood <= cost)
+		{
+			time += 45;
+		}
+	}
+
+}
