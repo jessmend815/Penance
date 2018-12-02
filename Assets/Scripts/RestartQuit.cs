e010d43a2594442fa76190b359713bf136abf1a3
Jesse Mendoza <33880932+jessmend815@users.noreply.github.com>
2018-12-02 12:44:28 -0800

    Restart & Quit Button

---
+++ Assets/Scripts/RestartQuit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RestartQuit : MonoBehaviour {
+
+    public void RestartScene()
+    {   
+        PlayerController.player.gameObject.transform.position = PlayerController.player.startPos;
+        PlayerController.player.gameObject.SetActive(true);
+        SceneManager.LoadScene("ColeScene");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
