e7b333a4d0440d393c8373738e921a5d7de71c4a
cdunham927 <cdunham927@gmail.com>
2018-12-02 23:55:41 -0800

    Dead enemies(all), goblin enemy working

---
+++ Assets/Scripts/EnemyTriggerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTriggerController : MonoBehaviour
+{
+    public float damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.isTrigger)
+        {
+            if (collision.tag == "Player")
+            {
+                collision.GetComponent<PlayerController>().TakeDamage(damage);
+            }
+        }
+    }
+}
